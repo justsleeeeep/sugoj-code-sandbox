f1dcfc9e0f0798e16e50f55d91a76a6a8838f11a
sugpepper <qc87672090@qq.com>
2026-06-02 03:00:36 -0700

    上次那个连接是成功 现在测试pull image是否可以

--- src/main/java/com/sug/sugojcodesandbox/docker/DockerDemo.java
+++ src/main/java/com/sug/sugojcodesandbox/docker/DockerDemo.java
@@ -1,14 +1,23 @@
 package com.sug.sugojcodesandbox.docker;
 
 import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.async.ResultCallback;
 import com.github.dockerjava.api.command.PingCmd;
+import com.github.dockerjava.api.command.PullImageCmd;
+import com.github.dockerjava.api.command.PullImageResultCallback;
+import com.github.dockerjava.api.model.AuthConfig;
+import com.github.dockerjava.api.model.PullResponseItem;
 import com.github.dockerjava.core.DockerClientBuilder;
 
 public class DockerDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         DockerClient dockerClient = DockerClientBuilder.getInstance().build();
-        PingCmd pingCmd=dockerClient.pingCmd();
-        pingCmd.exec();
+//        PingCmd pingCmd=dockerClient.pingCmd();
+//        pingCmd.exec();
+        String image="nginx";
+        PullImageCmd pullImageCmd =dockerClient.pullImageCmd(image);
+        PullImageResultCallback pullImageResultCallback = new PullImageResultCallback();
+        pullImageCmd.exec(pullImageResultCallback).awaitCompletion();
 
     }
 }
